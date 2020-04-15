60d32a87d46a7a3c89dd2556dddd75cc649685f6
ahmedkmadani <ahmedkamal543@gmail.com>
2020-04-15 14:48:52 +0300

    change db con for deployment

--- dashboard/config.py
+++ dashboard/config.py
@@ -3,7 +3,8 @@ import os
 class Config():
     SECRET_KEY = 'gentlecare'
     socket_location = "/var/run/mysqld/mysqld.sock"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/gentlecare"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/gentlecare"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@178.62.66.6/gentlecare"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
